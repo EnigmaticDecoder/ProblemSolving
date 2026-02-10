6713f5ba85cc211e2db1b6f96b14ff331c4f95ba
EnigmaticDecoder <mu952917@gmail.com>
2026-02-10 16:05:54 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,11 @@
+// Last updated: 2/10/2026, 4:05:53 PM
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> st = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(st.contains(nums[i])) return nums[i];
+            else st.add(nums[i]);
+        }
+        return -1;
+    }
+}
