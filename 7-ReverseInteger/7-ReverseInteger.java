05b822837bec650c95bb84eb9a06a063522b0dfb
EnigmaticDecoder <mu952917@gmail.com>
2026-02-10 15:29:39 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,22 @@
+// Last updated: 2/10/2026, 3:29:38 PM
+class Solution {
+    public int reverse(int x) {
+    int rev = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        x /= 10;
+
+        if (rev > Integer.MAX_VALUE / 10 || 
+           (rev == Integer.MAX_VALUE / 10 && digit > 7))
+            return 0;
+
+        if (rev < Integer.MIN_VALUE / 10 || 
+           (rev == Integer.MIN_VALUE / 10 && digit < -8))
+            return 0;
+
+        rev = rev * 10 + digit;
+    }
+    return rev;
+}
+}
