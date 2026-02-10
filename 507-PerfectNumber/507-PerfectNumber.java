863d804de22d1bdeb8e842a25921a698239e6af5
EnigmaticDecoder <mu952917@gmail.com>
2026-02-10 15:28:02 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,6 @@
+// Last updated: 2/10/2026, 3:28:01 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) ? true : false; 
+    }
+}
