ecc656683563e799df893852e88addeca6a1f697
EnigmaticDecoder <mu952917@gmail.com>
2026-02-10 15:27:59 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.java
@@ -0,0 +1,20 @@
+// Last updated: 2/10/2026, 3:27:58 PM
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        else if(nums[0]!=nums[1]) return nums[0];
+        else if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+            else if((nums[mid]==nums[mid+1] && mid%2==0)||(nums[mid]==nums[mid-1] && mid%2!=0)){
+                start = mid+1;
+            }
+            else end = mid-1;
+        }
+        return 0;
+}
+}
