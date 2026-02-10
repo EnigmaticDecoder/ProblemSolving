4161082fb8ebae7ffcd22886008b924bf43aeb59
EnigmaticDecoder <mu952917@gmail.com>
2026-02-10 15:28:33 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,15 @@
+// Last updated: 2/10/2026, 3:28:33 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int hash[] = new int[26];
+        for(int i = 0; i<s.length(); i++){
+            hash[s.charAt(i)-'a'] += 1;
+            hash[t.charAt(i)-'a'] -= 1; 
+        }
+        for(int i : hash){
+            if(i!=0) return false;
+        }
+        return true;
+    }
+}
