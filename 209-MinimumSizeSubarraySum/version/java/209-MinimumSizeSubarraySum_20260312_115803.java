1e7a0d2d3e342ebfc91f92aa3be513c36637721a
EnigmaticDecoder <mu952917@gmail.com>
2026-03-12 11:58:04 +0530

    Create 209-MinimumSizeSubarraySum

---
+++ 209-MinimumSizeSubarraySum/version/java/209-MinimumSizeSubarraySum_20260312_115803.java
@@ -0,0 +1,20 @@
+// Last updated: 3/12/2026, 11:58:03 AM
+1class Solution {
+2    public int minSubArrayLen(int target, int[] nums) {
+3        int n = nums.length;
+4        int sum = 0;
+5        int min = n+1;
+6        int l = 0;
+7        int r = 0;
+8        for(int i = 0; i<n; i++){
+9            if(sum<target) sum+=nums[i];
+10            while(sum>=target){
+11                min = Math.min(min,i-l+1);
+12                sum-=nums[l++];
+13            }
+14        }
+15        if(min == n+1) return 0;
+16        else return min;
+17    }
+18}
+19
