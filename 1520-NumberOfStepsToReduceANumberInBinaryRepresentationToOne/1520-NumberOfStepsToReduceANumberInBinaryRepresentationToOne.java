208213c678df23671d703c70af7d0314268b40ec
EnigmaticDecoder <mu952917@gmail.com>
2026-03-24 14:24:24 +0530

    Create 1520-NumberOfStepsToReduceANumberInBinaryRepresentationToOne

---
+++ 1520-NumberOfStepsToReduceANumberInBinaryRepresentationToOne/1520-NumberOfStepsToReduceANumberInBinaryRepresentationToOne.java
@@ -0,0 +1,17 @@
+// Last updated: 3/24/2026, 2:24:23 PM
+class Solution {
+    public int numSteps(String s) {
+        int step = 0;
+        int carry = 0;
+        for (int i = s.length() - 1; i > 0; i--) {
+            int bit = s.charAt(i) - '0';
+            if (bit + carry == 1) {
+                step += 2;
+                carry = 1;
+            } else {
+                step += 1;
+            }
+        }
+        return step + carry;
+    }
+}
