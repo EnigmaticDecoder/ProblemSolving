46de25f320cb75069e5358fcfba746ebed91f4c3
EnigmaticDecoder <mu952917@gmail.com>
2026-04-07 14:51:19 +0530

    Create 1768-MergeStringsAlternately

---
+++ 1768-MergeStringsAlternately/version/java/1768-MergeStringsAlternately_20260407_145118.java
@@ -0,0 +1,21 @@
+// Last updated: 4/7/2026, 2:51:18 PM
+1class Solution {
+2    public String mergeAlternately(String word1, String word2) {
+3        int n = word1.length();
+4        int m = word2.length();
+5        int len = n>m?m:n;
+6        int a = 0;
+7        int b = 0;
+8        String str = "";
+9        for(int i = 0; i<2*len; i++){
+10            if(i%2==0) str+= word1.charAt(a++);
+11            else str +=word2.charAt(b++);
+12        }
+13
+14        if(n==m) return str;
+15        else if(n>m){
+16           return str += word1.substring(a);
+17        } else  return str += word2.substring(b);
+18
+19    }
+20}
